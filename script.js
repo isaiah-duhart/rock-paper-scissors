d60b3f6cdd75cf57f7e6b284ba27915cd1529b76
Isaiah Duhart <isaiah.duhart.1@gmail.com>
2025-01-31 16:26:10 -0500

    Plays five games of rock paper scissors, displaying score in console

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const choices = ["rock", "paper", "scissors"]
+let userScore = 0
+let computerScore = 0
 
 function getComputerChoice(){
     const index = Math.floor(Math.random() * 3)
@@ -6,21 +8,23 @@ function getComputerChoice(){
     return choices[index]
 }
 
-function compareChoices(userChoice, computerChoice){
+function playRound(userChoice, computerChoice){
     const userChoiceIndex = choices.indexOf(userChoice)
     const computerChoiceIndex = choices.indexOf(computerChoice)
 
     switch (userChoiceIndex - computerChoiceIndex){
         case 0:
-            console.log("tie!")
+            console.log(`tie! ${userChoice} ties ${computerChoice}`)
             break
         case 1:
         case -2:
-            console.log("you win!")
+            console.log(`you win! ${userChoice} beats ${computerChoice}`)
+            userScore++
             break
         case -1:
         case 2:
-            console.log("you lose!")
+            console.log(`you lose! ${computerChoice} beats ${userChoice}`)
+            computerScore++
             break
         default:
             console.log(`Something went wrong...
@@ -30,14 +34,21 @@ function compareChoices(userChoice, computerChoice){
     }
 }
 
-while (true) {
-    const userChoice = prompt("enter rock, paper, or scissors:").toLowerCase()
+function playGame(){
+    for (let i = 0; i < 5; i++){
+        const userChoice = prompt("enter rock, paper, or scissors:").toLowerCase()
 
-    if (choices.includes(userChoice)){
-        const computerChoice = getComputerChoice()
-        console.log(computerChoice)
-        compareChoices(userChoice, computerChoice)
-    } else {
-        console.log(`${userChoice} is not rock, paper, or scissors, enter valid choice`)
+        if (choices.includes(userChoice)){
+            const computerChoice = getComputerChoice()
+            playRound(userChoice, computerChoice)
+        } else {
+            console.log(`${userChoice} is not rock, paper, or scissors, enter valid choice`)
+        }
     }
+
+    console.log(`game over!
+    you won ${userScore} rounds
+    computer won ${computerScore} rounds`)
 }
+
+playGame()
